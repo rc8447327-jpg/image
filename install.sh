dc0e60136cf22ab74f666c1a50c99afc2be49c74
The Joker <rc8447327@gmail.com>
2026-05-02 20:35:25 +0530

    Add installation script for MetaXtract

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+chmod +x metaxtract.py
+sudo cp metaxtract.py /usr/local/bin/metaxtract
+sudo chmod +x /usr/local/bin/metaxtract
+
+echo "[+] MetaXtract Installed"
+echo "[+] Run: metaxtract <file>"
